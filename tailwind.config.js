8030f506f0ec7abdfa8c760dc9359412b713dbcd
jtellis <jessetellis@gmail.com>
2021-02-03 19:10:41 -0500

    Purge unused styles in prod env

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: [],
+  /*
+    NODE_ENV=production must be set to activate purging
+  */
+  purge: [
+    './src/client/**/*.html',
+    './src/client/**/*.js'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
